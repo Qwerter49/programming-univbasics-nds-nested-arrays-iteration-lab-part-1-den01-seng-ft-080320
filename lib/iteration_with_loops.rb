b0032bdcd55200caa2b77ab79c6849bd1f9d5ca2
Jay Oliver <jaykamas@gmail.com>
2020-07-06 16:51:31 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,5 +1,15 @@
 def find_even_values(src)
   
+  count = 0 
+  tempArray = []
   
+  while count < src.length do 
+    if src[count].even? do
+      tempArray << src[count]
+      count += 1 
+    else 
+      count += 1 
+    end 
+      tempArray
 
 end
